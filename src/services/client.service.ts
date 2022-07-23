f6bcb1236394975393c64fc6a7b3ac6b789bebe1
AlanFernandes86 <josefernandes.alan@gmail.com>
2022-07-23 07:49:45 -0300

    refactor: clientService now calls the clientRepository instead of calling the ORM directly

--- src/services/client.service.ts
+++ src/services/client.service.ts
@@ -1,60 +1,17 @@
-import getDataSource from '../models/MySqlDataSource';
 import IClient from '../interfaces/IClient';
-import Account from '../models/entities/Account';
-import Client from '../models/entities/Client';
-import Wallet from '../models/entities/Wallet';
-import HttpError from '../shared/HttpError';
-import uuid from '../utils/uuid';
+import Client from '../domain/Client';
+import clientRepository from '../repository/client.repository';
 
 const setClient = async (iClient: IClient): Promise<Client> => {
-  try {
-    const dataSource = await getDataSource();
+  const newClient = await clientRepository.setClient(iClient);
 
-    const account = new Account();
-    account.balance = 0.0;
-
-    const wallet = new Wallet();
-
-    const client = new Client();
-    client.name = iClient.name;
-    client.username = iClient.username;
-    client.password = uuid.getPasswordHash(iClient.password);
-    client.account = account;
-    client.wallet = wallet;
-
-    const newClient = await dataSource.manager.save(client);
-
-    return newClient;
-  } catch (error) {
-    console.log(error);
-    throw new HttpError(500, 'Error ao cadastrar novo cliente');
-  }
+  return newClient;
 };
 
 const getClientById = async (clientId: number): Promise<Client> => {
-  try {
-    const dataSource = await getDataSource();
+  const client = await clientRepository.getClientById(clientId);
 
-    const client = await dataSource.manager.findOneOrFail(Client, {
-      where: {
-        id: clientId,
-      },
-      relations: {
-        wallet: {
-          walletStocks: {
-            stock: true,
-          },
-          client: true,
-        },
-        account: true,
-      },
-    });
-
-    return client;
-  } catch (error) {
-    console.log(error);
-    throw new HttpError(404, 'Cliente não encontrado.');
-  }
+  return client;
 };
 
 export default {
