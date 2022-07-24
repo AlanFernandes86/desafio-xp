df7045811a8e1704f5019ae844e03b7cdb3f4a88
AlanFernandes86 <josefernandes.alan@gmail.com>
2022-07-24 17:51:24 -0300

    dev(routes): add the middlewares in their respective routes

--- src/routes/auth.routes.ts
+++ src/routes/auth.routes.ts
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 import authController from '../controllers/auth.controller';
+import loginMiddleware from '../middlewares/login.middleware';
+import newClientMiddleware from '../middlewares/newClient.middleware';
 
 const router = Router();
 
-router.post('/new-client', authController.setClient);
-router.post('/login', authController.login);
+router.post('/new-client', newClientMiddleware, authController.setClient);
+router.post('/login', loginMiddleware, authController.login);
 
 export default router;
