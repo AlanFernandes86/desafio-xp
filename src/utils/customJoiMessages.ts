79059dda6291bc7fc91bb24fdd190c20f6459a8b
AlanFernandes86 <josefernandes.alan@gmail.com>
2022-07-24 17:51:03 -0300

    dev(util): add 2 new messages in customJoiMessages

--- src/utils/customJoiMessages.ts
+++ src/utils/customJoiMessages.ts
@@ -3,5 +3,7 @@ import Joi from 'joi';
 export default (key: string, min?: number): Joi.LanguageMessages => ({
   'number.base': `"${key}" deve ser um número.`,
   'number.min': `"${key}" não pode ser menor que ${min}.`,
+  'string.base': `"${key}" deve ser uma string.`,
+  'string.min': `"${key}" não pode ter menos que ${min} caracteres.`,
   'any.required': `"${key}" é obrigatório.'`,
 });
