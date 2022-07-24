b2d589125aa99dcf8f7b73dda399567eee7fb7bb
AlanFernandes86 <josefernandes.alan@gmail.com>
2022-07-24 17:38:31 -0300

    dev(routes): add the middlewares in theis respective routes

--- src/routes/investimentos.routes.ts
+++ src/routes/investimentos.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import investimentoController from '../controllers/investimentos.controller';
+import buyOrSellMiddleware from '../middlewares/buyOrSell.middleware';
 
 const router = Router();
 
-router.post('/comprar', investimentoController.buy);
-router.post('/vender', investimentoController.sell);
+router.post('/comprar', buyOrSellMiddleware, investimentoController.buy);
+router.post('/vender', buyOrSellMiddleware, investimentoController.sell);
 router.get('/ativos', investimentoController.listStocksSeparately);
 router.get('/ativos/:codAtivo', investimentoController.getStockByCodAtivo);
 
