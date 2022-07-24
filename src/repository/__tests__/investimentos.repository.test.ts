6441e126ec0903c63cb2eb3eeccdbdf8fe5d334b
AlanFernandes86 <josefernandes.alan@gmail.com>
2022-07-24 12:17:11 -0300

    test(investimentos): if service getStocks() returns all stocks correctly

---
+++ src/repository/__tests__/investimentos.repository.test.ts
@@ -0,0 +1,40 @@
+import * as MySqlDataSource from '../../models/MySqlDataSource';
+import * as brapi from '../../data/network/brapi.api';
+import TestHelper from '../../../tests/TestHelperSQLite';
+import initializeDatabase from '../../helpers/initializeDatabase';
+import brapApiMock from '../../../tests/brap.api.mock';
+
+describe('Testes da camada de investimentos da aplicação', () => {
+  let mockGetDataSource: jest.SpyInstance;
+  let mockBrapApi: jest.SpyInstance;
+
+  beforeAll(async () => {
+    await TestHelper.instance.setupTestDB();
+
+    mockGetDataSource = jest
+      .spyOn(MySqlDataSource, 'default')
+      .mockReturnValue(TestHelper.instance.getDataSource());
+
+    mockBrapApi = jest
+      .spyOn(brapi, 'default')
+      .mockReturnValue(brapApiMock());
+
+    await initializeDatabase();
+  }, 60000);
+
+  afterAll(async () => {
+    await TestHelper.instance.dropDB();
+    mockGetDataSource.mockClear();
+    mockBrapApi.mockClear();
+  }, 60000);
+
+  it(
+    '"getStocks()" - Testa se a função retorna todas a ações disponíveis na corretora.',
+    (done) => {
+      brapApiMock().then((list) => {
+        expect([]).toContainEqual(list);
+        done();
+      });
+    },
+  );
+});
