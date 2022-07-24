9053592b11172ec5e2c4e9c45ab6990bb7ca2267
AlanFernandes86 <josefernandes.alan@gmail.com>
2022-07-24 14:39:41 -0300

    test(investimentos): listStocksSeparately service.

--- src/services/__tests__/investimentos.service.test.ts
+++ src/services/__tests__/investimentos.service.test.ts
@@ -3,6 +3,10 @@ import * as brapi from '../../data/network/brapi.api';
 import TestHelper from '../../../tests/TestHelperSQLite';
 import initializeDatabase from '../../helpers/initializeDatabase';
 import brapApiMock from '../../../tests/brap.api.mock';
+import investimentosService from '../investimentos.service';
+import WalletStock from '../../models/entities/WalletStock';
+import IClient from '../../interfaces/IClient';
+import clientService from '../client.service';
 
 describe('Testes da camada de service(investimentos) da aplicação', () => {
   let mockGetDataSource: jest.SpyInstance;
@@ -20,6 +24,40 @@ describe('Testes da camada de service(investimentos) da aplicação', () => {
       .mockReturnValue(brapApiMock());
 
     await initializeDatabase();
+
+    const payload = {
+      name: 'alan Fernandes',
+      username: 'AjnfXP',
+      password: '123456',
+    } as IClient;
+
+    const newClient = await clientService.setClient(payload);
+
+    const dataSource = await TestHelper.instance.getDataSource();
+    await dataSource.createQueryBuilder().insert().into(WalletStock)
+      .values([
+        {
+          stockId: 10,
+          walletId: newClient.wallet.id,
+          quantity: 150,
+        },
+        {
+          stockId: 5,
+          walletId: newClient.wallet.id,
+          quantity: 200,
+        },
+        {
+          stockId: 30,
+          walletId: newClient.wallet.id,
+          quantity: 300,
+        },
+        {
+          stockId: 40,
+          walletId: newClient.wallet.id,
+          quantity: 500,
+        },
+      ])
+      .execute();
   }, 60000);
 
   afterAll(async () => {
@@ -31,18 +69,19 @@ describe('Testes da camada de service(investimentos) da aplicação', () => {
   it(
     '"listStocksSeparately()" - Testa se a função retorna todas a ações disponíveis na corretora separadas por "purchased" e "available".',
     (done) => {
-      brapApiMock().then((list) => {
-        expect([]).toContainEqual(list);
+      investimentosService.listStocksSeparately(1).then((list) => {
+        expect(list).toHaveProperty('purchased');
+        expect(list).toHaveProperty('available');
         done();
       });
     },
   );
 
   it(
-    '"listStocksSeparately()" - Testa se as ações "purchased" contém a quantidade comprada.',
+    '"listStocksSeparately()" - Testa se as ações em "purchased" contém a quantidade comprada.',
     (done) => {
-      brapApiMock().then((list) => {
-        expect([]).toContainEqual(list);
+      investimentosService.listStocksSeparately(1).then((list) => {
+        expect(list.purchased[0]).toHaveProperty('purchasedQuantity');
         done();
       });
     },
